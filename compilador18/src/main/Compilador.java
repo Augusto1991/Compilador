bdf7c7f7d94a1526044f94d7e07d65c15f7c8eb4
Augusto <apetruccelli@dokkogroup.com.ar>
2018-10-01 20:17:52 -0300

    Casos de prueba y ejecutable

--- compilador18/src/main/Compilador.java
+++ compilador18/src/main/Compilador.java
@@ -10,16 +10,17 @@ public class Compilador {
 
 	public static void main(String[] args) {
 		try {
-			String path = "prueba.txt";
-			 FileInputStream archivo = new FileInputStream(path);
-			 StringBuffer sb = new StringBuffer();
-			 while (archivo.available() != 0)
-				 sb.append((char) archivo.read());
-			 AnalizadorLexico al = new AnalizadorLexico(sb);
-			 archivo.close();
-			 Parser parser = new Parser(al);
-			 parser.run();
-			 System.out.println("Tabla de Simbolos: \n" +TablaSimbolos.imprimir());
+			String path = args[0];
+			//String path = "pruebas/case.txt";
+			FileInputStream archivo = new FileInputStream(path);
+			StringBuffer sb = new StringBuffer();
+			while (archivo.available() != 0)
+				sb.append((char) archivo.read());
+			AnalizadorLexico al = new AnalizadorLexico(sb);
+			archivo.close();
+			Parser parser = new Parser(al);
+			parser.run();
+			System.out.println("Tabla de Simbolos: \n" +TablaSimbolos.imprimir());
 		 } catch (Exception e) {}
 
 	}
